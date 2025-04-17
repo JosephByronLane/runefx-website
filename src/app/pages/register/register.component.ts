77f944088adea56837273bb44b3d341005064ba9
JosephByronLane <24197141@modelo.edu.mx>
2025-04-16 19:00:40 -0600

    made base form

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -4,13 +4,15 @@ import { FormsModule } from '@angular/forms';
 import { RButtonComponent } from '../../components/rbutton/rbutton.component';
 import { BackgroundVideoComponent } from '../../components/background-video/background-video.component';
 import { RouterModule } from '@angular/router';
+import { InfoBoxComponent } from '../../components/info-box/info-box.component';
+import { dccOptions  } from '../../data/dccOptions.json';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule, FormsModule, RButtonComponent, BackgroundVideoComponent, RouterModule],
+  imports: [CommonModule, FormsModule, RButtonComponent, BackgroundVideoComponent, RouterModule, InfoBoxComponent ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-
+  dccOptions = dccOptions;
 }
