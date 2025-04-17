b4d6773399a352ff98b17f5b3d1054438e7b6e39
JosephByronLane <24197141@modelo.edu.mx>
2025-04-16 19:18:36 -0600

    fleshing out form

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, Validators, ReactiveFormsModule, NgControl } from '@angular/forms';
 import { RButtonComponent } from '../../components/rbutton/rbutton.component';
 import { BackgroundVideoComponent } from '../../components/background-video/background-video.component';
 import { RouterModule } from '@angular/router';
@@ -9,10 +9,45 @@ import { dccOptions  } from '../../data/dccOptions.json';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule, FormsModule, RButtonComponent, BackgroundVideoComponent, RouterModule, InfoBoxComponent ],
+  imports: [CommonModule, FormsModule, RButtonComponent, BackgroundVideoComponent, RouterModule, InfoBoxComponent, ReactiveFormsModule, RButtonComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
   dccOptions = dccOptions;
+
+  registerForm: FormGroup;
+
+
+
+
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(15)]],
+      email: ['', [Validators.required, Validators.email]],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      dcc: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(8), Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(8), Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)]],
+    },
+    {
+      validators: this.validatePasswords
+    }
+  );
+  }
+
+  validatePasswords(group: FormGroup){
+    const password = group.get('password')?.value;
+    const confirmPassword = group.get('confirmPassword')?.value;
+    if (password !== confirmPassword) {
+      group.get('confirmPassword')?.setErrors({ mismatch: true });
+    } else {
+      group.get('confirmPassword')?.setErrors(null);
+    }
+  }
+
+  onSubmit(){
+    
+  }
 }
