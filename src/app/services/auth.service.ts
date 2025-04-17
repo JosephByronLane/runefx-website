5643a467c513d569b9f4e82a72e2499f42cadf79
JosephByronLane <24197141@modelo.edu.mx>
2025-04-16 21:31:48 -0600

    base auth module and env viles

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,36 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.dev';
+import { IRegisterUser } from '../interfaces/IRegisterUser';
+import { catchError, Observable, throwError } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  constructor() { }
+  
+  private apiUrl = environment.apiUrl;
+  
+  constructor(private http: HttpClient) { }
+
+  get isAuthenticated(): boolean {
+    // TODO: Implement authentication check
+    return true
+  }
+
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+
+  private handleError(error: any): Observable<never> {
+    console.error('An error occurred:', error);
+    return throwError(() => new Error('Something went wrong; please try again later.'));
+  }
+  
+  register(data: IRegisterUser): Observable<any> {
+    return this.http.post(`${this.apiUrl}/auth/register`, data, this.httpOptions).pipe(catchError(this.handleError))
+  }
+
+
 }
