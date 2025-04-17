b63f64ac5955a1dadac98360861e87c9932ae2de
JosephByronLane <24197141@modelo.edu.mx>
2025-04-16 18:24:46 -0600

    added base register form

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RButtonComponent } from '../../components/rbutton/rbutton.component';
+import { BackgroundVideoComponent } from '../../components/background-video/background-video.component';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, RButtonComponent, BackgroundVideoComponent, RouterModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
