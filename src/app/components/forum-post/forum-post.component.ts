07cdf6c36090ec6a9d6ef35ad42afcb22287d3f2
JosephByronLane <24197141@modelo.edu.mx>
2025-06-26 21:52:13 -0600

    Added warning box to the bottom of posts

--- src/app/components/forum-post/forum-post.component.ts
+++ src/app/components/forum-post/forum-post.component.ts
@@ -4,18 +4,20 @@ import { MarkdownModule, MarkdownService } from 'ngx-markdown';
 import { MarkdownComponent } from 'ngx-markdown';
 import { CommonModule } from '@angular/common';
 import { UtilsService } from '../../services/utils.service';
+import { IntermitentLoadingService } from '../../services/intermitent-loading.service';
+import { ErrorWarningOxComponent } from '../error-warning-ox/error-warning-ox.component';
 
 @Component({
   selector: 'app-forum-post',
   standalone: true,
-  imports: [MarkdownModule, MarkdownComponent, CommonModule],
+  imports: [MarkdownModule, MarkdownComponent, CommonModule, ErrorWarningOxComponent],
   templateUrl: './forum-post.component.html',
   styleUrl: './forum-post.component.css'
 })
 export class ForumPostComponent {
   @Input() post: IPostAPIResponse = {} as IPostAPIResponse;
 
-  constructor(private readonly markdownService: MarkdownService, public readonly utilsService: UtilsService){
+  constructor(private readonly markdownService: MarkdownService, public readonly utilsService: UtilsService, public readonly loadingService: IntermitentLoadingService){
 
   }
 
