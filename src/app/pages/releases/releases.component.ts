aec969d7eeae84acc8ff111eaf1b72e24a53cf16
JosephByronLane <24197141@modelo.edu.mx>
2025-07-01 16:35:49 -0600

    base release page

--- src/app/pages/releases/releases.component.ts
+++ src/app/pages/releases/releases.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { BackgroundVideoComponent } from '../../components/background-video/background-video.component';
+import { InfoBoxComponent } from '../../components/info-box/info-box.component';
+import { ShowcaseBoxComponent } from '../../components/showcase-box/showcase-box.component';
 
 @Component({
   selector: 'app-releases',
   standalone: true,
-  imports: [],
+  imports: [BackgroundVideoComponent, InfoBoxComponent, ShowcaseBoxComponent],
   templateUrl: './releases.component.html',
   styleUrl: './releases.component.css'
 })
