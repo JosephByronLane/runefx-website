49f2653e335d9057de5eefe936d3f4551ba00a10
JosephByronLane <24197141@modelo.edu.mx>
2025-07-05 14:27:13 -0600

    finished faq

--- src/app/pages/faq/faq.component.ts
+++ src/app/pages/faq/faq.component.ts
@@ -1,18 +1,24 @@
 import { Component } from '@angular/core';
 import { BackgroundVideoComponent } from '../../components/background-video/background-video.component';
 import { AccordionComponent } from '../../components/accordion/accordion.component';
+import { CommonModule } from '@angular/common';
+import { IFaqItem } from '../../interfaces/IFaqItem';
+import { allAccordionItems as items } from '../../data/faqItems.json';
+import { InfoBoxComponent } from '../../components/info-box/info-box.component';
+import { RButtonComponent } from '../../components/rbutton/rbutton.component';
+
 
 @Component({
   selector: 'app-faq',
   standalone: true,
-  imports: [BackgroundVideoComponent, AccordionComponent],
+  imports: [BackgroundVideoComponent, AccordionComponent, CommonModule, InfoBoxComponent, RButtonComponent],
   templateUrl: './faq.component.html',
   styleUrl: './faq.component.css'
 })
 export class FaqComponent {
-accordionItems = [
-  { title: 'asdasd', content: 'aaaaaaaaaa' },
-  { title: 'asdasdasd', content: 'bbbbbbbbbb' },
-  { title: 'asdasdasd', content: 'ccccccccccccccc' }
-];
+  
+  accordionItems: IFaqItem[] = items
+
+    
+  
 }
