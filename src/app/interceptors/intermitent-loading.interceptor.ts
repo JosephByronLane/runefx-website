80bbcd22f8998f250eb93fbc46d281f339a007e9
JosephByronLane <24197141@modelo.edu.mx>
2025-07-14 18:42:08 -0600

    added pending requests so http load and api requests get served together

--- src/app/interceptors/intermitent-loading.interceptor.ts
+++ src/app/interceptors/intermitent-loading.interceptor.ts
@@ -5,11 +5,13 @@ import { finalize } from 'rxjs/operators';
 import { IntermitentLoadingService } from '../services/intermitent-loading.service';
 import { InitialLoadingService } from '../services/initial-loading.service';
 
-
+let requestsPending = 0;
 
 export function intermitentLoadingInterceptor(req:HttpRequest<any>, next:HttpHandlerFn): Observable<HttpEvent<any>>{
   const intermitentLoading = inject(IntermitentLoadingService);
   const initialLoading = inject(InitialLoadingService)
+
+  requestsPending++;
 
   intermitentLoading.setLoadingTrue()
   initialLoading.setisAPIRequestTrue()
@@ -17,18 +19,29 @@ export function intermitentLoadingInterceptor(req:HttpRequest<any>, next:HttpHan
   return next(req).pipe(
     finalize(() => {
       try {        
+
+        requestsPending--;
+
+        if (requestsPending<=0){
           setTimeout(()=>{
             intermitentLoading.setLoadingFalse();
             intermitentLoading.hideLoadingScreen();
             initialLoading.setisAPIRequestFalse();
             initialLoading.hideInitialLoadingScreen();
           }, 500)
+        }
+
       } catch (error) {
         console.error(error)
-        intermitentLoading.setLoadingFalse();
-        intermitentLoading.hideLoadingScreen();
-        initialLoading.setisAPIRequestFalse();
-        initialLoading.hideInitialLoadingScreen();
+
+        if(requestsPending <= 0 ){
+          intermitentLoading.setLoadingFalse();
+          intermitentLoading.hideLoadingScreen();
+          initialLoading.setisAPIRequestFalse();
+          initialLoading.hideInitialLoadingScreen();
+        }
+
+
       }
     })
   );
